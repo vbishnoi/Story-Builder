071e902822f4096be1d7bfd31a5e4a6ac7d9b4f4
Tuan Vu <contact@tuanva.co.uk>
2013-04-04 16:18:10 +0100

    GUI updates

--- storybuilder/src/controller/DataController.java
+++ storybuilder/src/controller/DataController.java
@@ -5,21 +5,17 @@ import java.io.File;
 import org.w3c.dom.Document;
 
 public class DataController {
-	
-	public static void readData(){
-		
-	}
-	
-	public static void saveData(){
-		
-	}
-	
-	public static Document readXml(File file){
-		return null;
-	}
-	
-	public static void saveXml(Document doc, File file){
-		
-	}
 
+    public static void readData() {
+    }
+
+    public static void saveData() {
+    }
+
+    public static Document readXml(File file) {
+        return null;
+    }
+
+    public static void saveXml(Document doc, File file) {
+    }
 }
--- storybuilder/src/controller/Main.java
+++ storybuilder/src/controller/Main.java
@@ -4,38 +4,27 @@ import java.io.File;
 
 import org.w3c.dom.Document;
 
-public class Main 
-	{
+public class Main {
 
-	public static void readData(){
-		
-	}
-	
-	public static void saveData(){
-		
-	}
-	
-	public static Document readXml(File file){
-		return null;
-	}
-	
-	public static void saveXml(Document doc, File file){
-		
-	}
-	
-	public static void loadSound(){
-		
-	}
-	
-	public static void loadImage(){
-		
-	}
-	
-	public static void readImage(){
-		
-	}
+    public static void readData() {
+    }
 
-	
-	
-	
+    public static void saveData() {
+    }
+
+    public static Document readXml(File file) {
+        return null;
+    }
+
+    public static void saveXml(Document doc, File file) {
+    }
+
+    public static void loadSound() {
+    }
+
+    public static void loadImage() {
+    }
+
+    public static void readImage() {
+    }
 }
